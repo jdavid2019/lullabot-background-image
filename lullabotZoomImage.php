90dd7b75d8481119343da05476bb020f7e945aa1
lmendoza27 <luigui_mendoza@hotmail.com>
2021-10-02 11:59:50 -0500

    ok

--- lullabotZoomImage.php
+++ lullabotZoomImage.php
@@ -24,31 +24,66 @@ class lullabotZoomImage {
      * @return ClaviskaSimpleImage if params are correct else a error message.
      */
     public function generateDynamicBackgroundImages() {
-        if(isset($_GET['name']) && isset($_GET['role'])) {
-            $this->getClaviskaImage()
-                ->fromFile('assets/img/blank.png')
-                ->autoOrient()                           ///< adjust orientation
-                //->resize(630, 379)
-                ->text("NAME: " . strtoupper($_GET['name']), [
-                        'fontFile' => 'assets/font/mullishregular.ttf',
-                        'size' => 298 ,
-                        'color' => '#000000',
-                        'anchor' => 'top start',
-                        'xOffset' => 40,
-                        'yOffset' => 0,
-                    ]
-                )
-                ->text("ROLE: " . strtoupper($_GET['role']), [
-                        'fontFile' => 'assets/font/mullishregular.ttf',
-                        'size' => 198 ,
-                        'color' => '#000000',
-                        'anchor' => 'top start',
-                        'xOffset' => 40,
-                        'yOffset' => 340,
-                    ]
-                )
-                ->overlay('assets/img/lullabot-social-og-image.png', 'end center',1,0,725) ///< dni number
-                 ->toScreen();
+        if(isset($_GET['name']) && isset($_GET['role']) && isset($_GET['view'])) {
+          if($_GET['view'] == '1' || $_GET['view'] == '2' ) {
+            if($_GET['view'] == '1') {
+              $this->getClaviskaImage()
+                  ->fromFile('assets/img/'. $_GET['view'] . '.png')
+                  ->text(ucwords($_GET['name']), [
+                          'fontFile' => 'assets/font/freight_big_medium.otf',
+                          'size' => 72,
+                          'color' => '#000000',
+                          'anchor' => 'end left',
+                          'xOffset' => -565,
+                          'yOffset' => 350,
+                          'calculateOffsetFromEdge' => true,
+                          'baselineAlign' => true,
+                      ]
+                  )
+                  ->text(ucwords($_GET['role']), [
+                          'fontFile' => 'assets/font/freight_big_medium.otf',
+                          'size' => 54,
+                          'color' => '#000000',
+                          'anchor' => 'end left',
+                          'xOffset' => -600,
+                          'yOffset' => 450,
+                          'calculateOffsetFromEdge' => true,
+                          'baselineAlign' => true,
+                      ]
+                  )
+                  ->toScreen();
+            }elseif ($_GET['view'] == '2') {
+              $this->getClaviskaImage()
+                  ->fromFile('assets/img/'. $_GET['view'] . '.png')
+                  ->textBox($_GET['name'], array(
+                      'fontFile' => 'assets/font/freight_big_medium.otf',
+                      'size' => 70,
+                      'color' => '#FFFFFF',
+                      'anchor' => 'end left',
+                      'xOffset' => 394,
+                      'yOffset' => 350,
+                      'leading' => 30,
+                      'justify' => 'justify',
+                      'width' => 1000,
+                  ))
+                  ->textBox(strtoupper($_GET['role']), array(
+                      'fontFile' => 'assets/font/freight_big_medium.otf',
+                      'size' => 36,
+                      'color' => '#FFFFFF',
+                      'anchor' => 'end left',
+                      'xOffset' => 394,
+                      'yOffset' => 425,
+                      'leading' => 30,
+                      'justify' => 'justify',
+                      'width' => 1000,
+                  ))
+                  ->toScreen();
+            }
+
+          } else {
+            echo "<h1>VIEW TYPE NOT FOUND</h1><h2>Created by Joseph David</h2>";
+          }
+
         } else {
             echo "<h1>URL NOT FOUND</h1><h2>Created by Joseph David</h2>";
         }
